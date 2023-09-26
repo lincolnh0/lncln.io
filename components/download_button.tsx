150c3780ee4f5be4f6fb74fbddc47b72a01219d6
lincolnh0 <me@lincolnh0.com>
2023-09-26 23:04:04 +0800

    Update button class

--- components/download_button.tsx
+++ components/download_button.tsx
@@ -6,7 +6,7 @@ type DownloadButtonProp = {
 }
 export default function DownloadButton({path} : DownloadButtonProp) {
   return (
-      <a className={"py-2 px-4 rounded shadow w-fit bg-gray-500 text-gray-100 font-bold hover:bg-gray-600 hover:text-gray-0"}
+      <a className={"py-2 px-4 inline-block rounded shadow w-fit bg-gray-500 text-gray-100 font-bold hover:bg-gray-600 hover:text-gray-0"}
          href={ "/markdowns/" + path }><FontAwesomeIcon className={"pr-2"} icon={faDownload}/>Download .md</a>
   )
 }
--- components/home_button.tsx
+++ components/home_button.tsx
@@ -3,7 +3,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function HomeButton() {
   return (
-      <a className={"py-2 px-4 rounded shadow w-fit bg-gray-200 text-gray-600 font-bold hover:bg-gray-300 hover:text-gray-700"}
+      <a className={"py-2 px-4 inline-block rounded shadow w-fit bg-gray-200 text-gray-600 font-bold hover:bg-gray-300 hover:text-gray-700"}
          href={"/"}><FontAwesomeIcon className={"pr-2"} icon={faArrowLeft}/>Back to home</a>
   )
 }
