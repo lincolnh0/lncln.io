7148cf0d084f240e7379fcd0060b0ba88b5d5a30
lincolnh0 <me@lincolnh0.com>
2024-07-14 10:58:52 +0100

    Update home button + import FA css on root document

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,31 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import {config} from '@fortawesome/fontawesome-svg-core';
 
-const inter = Inter({ subsets: ['latin'] })
+config.autoAddCss = false; /* eslint-disable import/first */
+
+
+import './globals.css'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'lncln.io',
-  description: 'portfolio and development blog',
+    title: 'lncln.io',
+    description: 'portfolio and development blog',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
--- components/home_button.tsx
+++ components/home_button.tsx
@@ -1,13 +1,16 @@
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft, faHouse} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function HomeButton() {
-  return (
+    return (
 
-      <a className={"btn btn-ghost my-4"}
-         href={"/"}>
-            <FontAwesomeIcon className={"pr-2"} icon={faArrowLeft}/>
-        Back to home
-      </a>
-  )
+        <a className={"btn btn-ghost my-4"}
+           href={"/"}>
+            <FontAwesomeIcon className={"pr-2"} size={"sm"} icon={faHouse}/>
+            Back to home
+
+        </a>
+
+
+    )
 }
