6f2b80efda9a73a6aaa410834018a465ff74f733
Lincoln Ho <15695012+lincolnh0@users.noreply.github.com>
2025-01-13 17:26:37 +0000

    Add umami tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ config.autoAddCss = false; /* eslint-disable import/first */
 import './globals.css'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -25,6 +26,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+        <Script src="https://cloud.umami.is/script.js" data-website-id="3ee7558b-1a42-4190-b237-5ff95c5468d2"></Script>   
         <body className={inter.className}>{children}</body>
         </html>
     )
